d3fb0474c2e3076c596ee46b54a45d3dfd481773
Keigo Tanigawa <taniigo@net77-dhcp100.sfc.keio.ac.jp>
2022-05-23 16:39:41 +0900

    mkdir src/*

--- src/printf/ft_printf.h
+++ src/printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: keitanig <keitanig@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 21:48:11 by keitanig          #+#    #+#             */
-/*   Updated: 2022/04/12 16:07:49 by keitanig         ###   ########.fr       */
+/*   Updated: 2022/05/23 16:33:48 by keitanig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include <stdarg.h>
 # include <limits.h>
-# include "libft.h"
+# include "../../src/libft/libft.h"
 
 int	ft_printf(const char *format, ...);
 int	parse_converting_specifier(const char format, va_list arg);
